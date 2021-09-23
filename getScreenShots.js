19cc618b9cb7adcfd1eb728be0c0ddbcbfb24229
Luana Cristini <luana@thecyborg.xyz>
2021-09-23 08:56:13 -0300

    Update getScreenShots.js

--- getScreenShots.js
+++ getScreenShots.js
@@ -5,7 +5,7 @@ const { urls } = require('./sites.js');
 
 // Put your custom dimension and name here
 const devices = [
-     { name: '1920x1080', width: 1920, height: 1080 },
+    { name: '1920x1080', width: 1920, height: 1080 },
     { name: '1366x768', width: 1366, height: 768 },
     { name: '1360x768', width: 1360, height: 768 },
     { name: '1280x800', width: 1280, height: 800 },
@@ -19,8 +19,7 @@ async function setViewports(device, url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.waitForTimeout(500);
-    await page.goto(url.link, 
-    {waitUntil: 'networkidle0' });
+    await page.goto(url.link, {waitUntil: 'networkidle0' });
 
     // Setting-up login
     await page.type('#copy selector', 'user.name');
@@ -32,8 +31,7 @@ async function setViewports(device, url) {
         page.waitForNavigation({ waitUntil: 'networkidle0' }),
       ]);
 
-    await page.goto(url.link, 
-        {waitUntil: 'networkidle0' });
+    await page.goto(url.link, {waitUntil: 'networkidle0' });
 
     // Setting-up viewports 
     await page.setViewport({
@@ -45,15 +43,15 @@ async function setViewports(device, url) {
 
 // Directory Create if not exist
 async function getScreenshots(device, url, page, browser) {
-   var new_location = './Sample/' + device.name+'('+ device.width+'-'+device.height +')';
+   var new_location = `./Sample/ ${device.name}`;
    fs.mkdir(new_location, function (err) {
         if (err) { //console.log(err) 
         }
    });
 
     await page.screenshot({
-        path: new_location +'/'+ url.name +`_${Date.now()}.png`,
-        fullPage: true,        
+        path: `${new_location}/${url.name}-${Date.now()}.png`,
+        fullPage: true,
     });
     browser.close();
 }
@@ -63,12 +61,12 @@ const name = Array.from(new Set(devices.map(item => item.name)));
 const link = Array.from(new Set(urls.map(item => item.link)));
 
 async function getUrlAndResolutions(devices, urls) {
-    var cont = 0;
+    var cont=0;
     for (let device of devices) {
         for (let url of urls) {
             await setViewports(device, url);
-            console.info('Resolução:', name[cont] + ', Página: ' + link);
-            cont = cont + 1;
+            console.info(`Resolução: ${name[cont]}, Página: ${link}`);
+            cont++;
         }
     }
 }
