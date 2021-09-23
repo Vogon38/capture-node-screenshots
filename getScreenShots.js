ad977953500b16fc341fb62d7260625f3afe1056
Luana Cristini <luana@thecyborg.xyz>
2021-09-22 21:33:06 -0300

    Update getScreenShots.js

--- getScreenShots.js
+++ getScreenShots.js
@@ -5,14 +5,14 @@ const { urls } = require('./sites.js');
 
 // Put your custom dimension and name here
 const devices = [
-    { name: 'Highest', width: 1920, height: 1080 },
-    { name: 'Laptop-1', width: 1366, height: 768 },
-    { name: 'Laptop-2', width: 1360, height: 768 },
-    { name: 'Small-Desktop-1', width: 1280, height: 800 },
-    { name: 'Small-Desktop--2', width: 1024, height: 768 },
-    { name: 'Ipad-Tab', width: 768, height: 1024 },
-    { name: 'Android', width: 360, height: 640 },
-    { name: 'Iphone', width: 375, height: 667 },
+     { name: '1920x1080', width: 1920, height: 1080 },
+    { name: '1366x768', width: 1366, height: 768 },
+    { name: '1360x768', width: 1360, height: 768 },
+    { name: '1280x800', width: 1280, height: 800 },
+    { name: '1024x768', width: 1024, height: 768 },
+    { name: '768x1024', width: 768, height: 1024 },
+    { name: '360x640', width: 360, height: 640 },
+    { name: '375x667', width: 375, height: 667 },
 ];
 
 async function setViewports(device, url) {
@@ -58,11 +58,17 @@ async function getScreenshots(device, url, page, browser) {
     browser.close();
 }
 
+// Mapping Array
+const name = Array.from(new Set(devices.map(item => item.name)));
+const link = Array.from(new Set(urls.map(item => item.link)));
+
 async function getUrlAndResolutions(devices, urls) {
+    var cont = 0;
     for (let device of devices) {
         for (let url of urls) {
             await setViewports(device, url);
-            console.log(device, url);
+            console.info('Resolução:', name[cont] + ', Página: ' + link);
+            cont = cont + 1;
         }
     }
 }
